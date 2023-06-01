81a34f397c59e3195e759758f695150ace77ee45
Torkel Rogstad <torkel@rogstad.io>
2023-06-01 08:47:08 +0200

    move around code

--- cmd/main.go
+++ cmd/main.go
@@ -5,7 +5,7 @@ import (
 	"os"
 
 	// Blank-import the function package so the init() runs
-	_ "github.com/barebitcoin/logdna-gcp/logdna"
+	_ "github.com/barebitcoin/logdna-gcp"
 
 	"github.com/GoogleCloudPlatform/functions-framework-go/funcframework"
 )
